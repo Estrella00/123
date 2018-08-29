05d568e2ea5aeae18a0c406485e62b484175296c
宛鈴 洪 <sunnyforest00@gmail.com>
2018-08-29 11:35:05 +0800

    add
    all

---
+++ 1052054_hw1/1052054_hw1/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace _1052054_hw1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.simpleOpenGlControl1 = new Tao.Platform.Windows.SimpleOpenGlControl();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // simpleOpenGlControl1
+            // 
+            this.simpleOpenGlControl1.AccumBits = ((byte)(0));
+            this.simpleOpenGlControl1.AutoCheckErrors = false;
+            this.simpleOpenGlControl1.AutoFinish = false;
+            this.simpleOpenGlControl1.AutoMakeCurrent = true;
+            this.simpleOpenGlControl1.AutoSwapBuffers = true;
+            this.simpleOpenGlControl1.BackColor = System.Drawing.Color.Black;
+            this.simpleOpenGlControl1.ColorBits = ((byte)(32));
+            this.simpleOpenGlControl1.DepthBits = ((byte)(16));
+            this.simpleOpenGlControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.simpleOpenGlControl1.Location = new System.Drawing.Point(0, 0);
+            this.simpleOpenGlControl1.Name = "simpleOpenGlControl1";
+            this.simpleOpenGlControl1.Size = new System.Drawing.Size(972, 652);
+            this.simpleOpenGlControl1.StencilBits = ((byte)(0));
+            this.simpleOpenGlControl1.TabIndex = 0;
+            this.simpleOpenGlControl1.Load += new System.EventHandler(this.simpleOpenGlControl1_Load);
+            this.simpleOpenGlControl1.Paint += new System.Windows.Forms.PaintEventHandler(this.simpleOpenGlControl1_Paint);
+            this.simpleOpenGlControl1.Resize += new System.EventHandler(this.simpleOpenGlControl1_Resize);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Enabled = false;
+            this.pictureBox1.ErrorImage = null;
+            this.pictureBox1.Image = global::_1052054_hw1.Properties.Resources.飛碟1;
+            this.pictureBox1.Location = new System.Drawing.Point(21, 197);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(110, 102);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.Font = new System.Drawing.Font("微軟正黑體", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.ForeColor = System.Drawing.Color.Transparent;
+            this.button1.Location = new System.Drawing.Point(790, 534);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(67, 50);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "stop";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Black;
+            this.button2.Font = new System.Drawing.Font("微軟正黑體", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button2.ForeColor = System.Drawing.Color.Transparent;
+            this.button2.Location = new System.Drawing.Point(863, 534);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(57, 50);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "go";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Black;
+            this.button3.Font = new System.Drawing.Font("微軟正黑體", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button3.ForeColor = System.Drawing.Color.Transparent;
+            this.button3.Location = new System.Drawing.Point(790, 478);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(131, 50);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "UFO!!";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Black;
+            this.button4.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button4.ForeColor = System.Drawing.Color.Transparent;
+            this.button4.Location = new System.Drawing.Point(790, 590);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(130, 40);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "disappear";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Black;
+            this.button5.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button5.ForeColor = System.Drawing.Color.Transparent;
+            this.button5.Location = new System.Drawing.Point(927, 478);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(27, 152);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "EXIT";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(972, 652);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.simpleOpenGlControl1);
+            this.Name = "Form1";
+            this.Text = "北斗七星";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Tao.Platform.Windows.SimpleOpenGlControl simpleOpenGlControl1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ 1052054_hw1/1052054_hw1/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tao.OpenGl;
+
+namespace _1052054_hw1
+{
+    public partial class Form1 : Form
+    {
+        int dx = -8, dy = -10;
+       
+        public Form1()
+        {
+            InitializeComponent();
+            this.simpleOpenGlControl1.InitializeContexts();
+        }
+
+        private void simpleOpenGlControl1_Load(object sender, EventArgs e)
+        {
+            Gl.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+            Gl.glMatrixMode(Gl.GL_PROJECTION);
+            Gl.glLoadIdentity();
+            Glu.gluOrtho2D(0.0f, this.simpleOpenGlControl1.Size.Width, 0.0f, this.simpleOpenGlControl1.Size.Height);
+            Gl.glViewport(0, 0, this.simpleOpenGlControl1.Size.Width, this.simpleOpenGlControl1.Size.Height);
+
+            pictureBox1.Visible = false;
+        }
+
+        private void simpleOpenGlControl1_Paint(object sender, PaintEventArgs e)
+        {
+            Random rn = new Random();
+         
+            Gl.glPointSize(2.0f);
+            Gl.glColor3ub(255, 255, 255);
+
+            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT);
+
+           
+            for (int i = 0; i < 300; i++)
+            {   
+                //-----星星大小隨機----//
+                Gl.glPointSize(rn.Next(0,2));  //0~2之間大小, pointsize指令放在begin&end裡無效
+                Gl.glBegin(Gl.GL_POINTS);
+                Gl.glVertex2i(rn.Next(0, this.simpleOpenGlControl1.Size.Width),
+                              rn.Next(0, this.simpleOpenGlControl1.Size.Height));
+                Gl.glEnd();
+            }
+            Gl.glPointSize(5.0f); 
+
+            Gl.glBegin(Gl.GL_POINTS);
+
+
+            Gl.glColor3f(1.0f, 0.0f, 0.0f); // 紅
+
+            Gl.glVertex2i(380, 290);
+
+
+            Gl.glColor3f(1.0f, 0.5f, 0.0f); // 橙
+
+            Gl.glVertex2i(410, 230);
+
+
+            Gl.glColor3f(1.0f, 1.0f, 0.0f); // 黃
+
+            Gl.glVertex2i(330, 165);
+
+
+            Gl.glColor3f(0.0f, 1.0f, 0.0f); // 綠
+
+            Gl.glVertex2i(285, 200);
+
+
+            Gl.glColor3f(0.0f, 0.0f, 1.0f); // 藍
+
+            Gl.glVertex2i(220, 180);
+
+
+            Gl.glColor3f(1.0f, 0.0f, 1.0f); // 靛
+
+            Gl.glVertex2i(165, 170);
+
+
+            Gl.glColor3f(0.5f, 0.0f, 1.0f); // 紫
+
+            Gl.glVertex2i(110, 110);
+
+
+            Gl.glEnd();
+               
+          
+        }
+        //------飛碟-----//
+
+        //----出現-----//
+        private void button3_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Visible =true;
+        }
+
+        //----出現-----//
+        private void button4_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Visible = false;
+        }
+
+        //----出現-----//
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+           timer1.Enabled = false;
+          
+        }
+
+        //----出現-----//
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            pictureBox1.Top += dy;
+            pictureBox1.Left += dx;
+            //---怕超出格子---碰撞機制//
+            if (pictureBox1.Left < 0)
+            {
+                dx = -1 * dx;
+            }
+            if (pictureBox1.Top < 0)
+            {
+                dy = -1 * dy;
+            }
+            if (pictureBox1.Bottom > this.simpleOpenGlControl1.Height)
+            {
+                dy = -1 * dy;
+            }
+            if (pictureBox1.Right > this.simpleOpenGlControl1.Width)
+            {
+                dx = -1 * dx;
+            }
+          
+
+        }
+
+        private void simpleOpenGlControl1_Resize(object sender, EventArgs e)
+        {
+            Gl.glMatrixMode(Gl.GL_PROJECTION);
+            Gl.glLoadIdentity();
+            Glu.gluOrtho2D(0.0f, this.simpleOpenGlControl1.Size.Width, 0.0f, this.simpleOpenGlControl1.Size.Height);
+            Gl.glViewport(0, 0, this.simpleOpenGlControl1.Size.Width, this.simpleOpenGlControl1.Size.Height);
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Application.Exit(); //結束此應用程式
+        }
+
+       
+
+        
+
+        
+
+        
+
+        
+
+       
+    }
+}
